97fdccf8d35770e66e8af54ae4d500b0d29a9146
Arko Chakraborty <arkoo1437@gmail.com>
2023-07-09 03:14:57 +0530

    added if in new code

--- src/main/java/gitfirst/App.java
+++ src/main/java/gitfirst/App.java
@@ -3,6 +3,11 @@ package gitfirst;
 public class App {
 
 	public static void main(String[] args) {
+		
+		if(true==true)
+		{
+			System.out.print("Change git uhhu");
+		}
 		// TODO Auto-generated method stub
  System.out.println("git uhhu");
 	}
