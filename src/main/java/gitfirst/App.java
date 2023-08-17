d99ddfc7e23de8e0edf6e7242af0f6e0811a9d2d
arko1437 <71840351+arko1437@users.noreply.github.com>
2023-08-18 01:12:16 +0530

    Update App.java

--- src/main/java/gitfirst/App.java
+++ src/main/java/gitfirst/App.java
@@ -6,7 +6,7 @@ public class App {
 		
 		if(true==true)
 		{
-			System.out.print("Change git uhhu");
+			System.out.print("Change git uhhu rinku pinku");
 		}
 		// TODO Auto-generated method stub
  System.out.println("git uhhu");
